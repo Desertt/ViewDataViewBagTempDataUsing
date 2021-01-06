73c6006de4a618d93d02005fbc0fda080fae32c9
mustafa.dagtekin <mustafa.dagtekin@outlook.com>
2021-01-07 02:01:42 +0300

    ViewDataViewBagTempDataUsing

--- ViewDataViewBagTempDataUsing/Controllers/HomeController.cs
+++ ViewDataViewBagTempDataUsing/Controllers/HomeController.cs
@@ -14,6 +14,26 @@ namespace ViewDataViewBagTempDataUsing.Controllers
             ViewData["Ad"] = "Mustafa DAĞTEKİN";
             ViewBag.Adi = "Kazım Miraç DAĞTEKİN";
             TempData["Ad"] = "Merve DAĞTEKİN";
+            return RedirectToAction("Ornek");
+        }
+
+        public ActionResult Ornek()
+        {
+            var ViewDataVeri = ViewData["Ad"];
+            if (ViewDataVeri==null)
+            {
+                ViewData["Ad"] = "Bu Controllerdan Veri Gelmemiştir. ";
+            }
+            var ViewBagVeri = ViewBag.Adi;
+            if (ViewBagVeri == null)
+            {
+                ViewBag.Adi= "Bu Controllerdan Veri Gelmemiştir. ";
+            }
+            var TempDataVeri = TempData["Ad"];
+            if (TempDataVeri == null)
+            {
+                TempData["Ad"] = "Bu Controllerdan Veri Gelmemiştir. ";
+            }
             return View();
         }
     }
